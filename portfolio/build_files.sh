6a695f433b9d71467b9e5fd918dc5b2f4d934e12
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2026-04-03 18:53:04 +0600

    modify the  build files

--- portfolio/build_files.sh
+++ portfolio/build_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
+python manage.py makemigrations
 python manage.py migrate --noinput
+python manage.py loaddata core/fixtures/initial_data.json
 python manage.py collectstatic --noinput
